d840b135b17c715312fe34adabdfb14587c77b6a
hussamh10 <hussamh10@gmail.com>
2018-03-09 20:52:05 +0500

    changed generator to support data

--- generator.py
+++ generator.py
@@ -1,31 +1,36 @@
 import numpy as np
 from cv2 import imread
+import os
+import cv2
 
 
-def getImage(i, source, main_dir):
-    name = str(i) + '.jpg'
 
-    print(main_dir + source + '\\' + name)
+def getImage(i, source, main_dir, ext, size):
+    name = str(i) + ext
+    print(main_dir + source + '/' + name)
 
-    img = imread(main_dir + source + '\\' + name, 0)
+    path = os.path.join(main_dir, source , name)
+    img = imread(path, 0)
+
+    img = cv2.resize(img, size)
     img = img.reshape((img.shape[0], img.shape[1], 1))
-    img = img.reshape(224, 224, 1)
+
     img = img.astype('float32')
     img /= 255
+
     return img
 
 def generate(size):
-    dir = 'data\\'
-    src_x1 = '1\\'
-    src_x2 = '2\\'
-    src_x3 = '3\\'
+    dir = os.path.join('..', 'data', 'data', '0')
+    frame_ext = '.jpg'
+    frame_size = (953, 273)
 
-    i = 0
+    i = 1
 
     while i < size:
-        x1 = getImage(i, src_x1, dir)
-        x2 = getImage(i, src_x2, dir)
-        x3 = getImage(i, src_x3, dir)
+        x1 = getImage(i, '', dir, frame_ext, frame_size)
+        x2 = getImage(i+1, '', dir, frame_ext, frame_size)
+        x3 = getImage(i+2, '', dir, frame_ext, frame_size)
 
         x1 = x1.reshape((1, x1.shape[0], x1.shape[1], x1.shape[2]))
         x2 = x2.reshape((1, x2.shape[0], x2.shape[1], x2.shape[2]))
